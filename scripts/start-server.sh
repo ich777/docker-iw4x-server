9889222ae52f7e0ea2b1c62282099937b05a566d
ich777 <ich777@noreply.example.org>
2020-03-21 19:43:16 +0100

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -25,5 +25,35 @@ if [ ! -f ${DATA_DIR}/iw4x.exe ]; then
 	rm ${DATA_DIR}/iw4x_dlc.zip
 fi
 
+echo "---Checking if gamefiles are in place---"
+if [ ! -f ${DATA_DIR}/main/iw_00.iwd ]; then
+	echo "---Gamefiles not found, putting server into sleep mode---"
+	sleep infinity
+else
+	echo "---Gamefiles found!---"
+fi
+
+export WINEARCH=win32
+export WINEPREFIX=/iw4x/WINE32
+echo "---Checking if WINE workdirectory is present---"
+if [ ! -d ${DATA_DIR}/WINE32]; then
+	echo "---WINE workdirectory not found, creating please wait...---"
+	mkdir ${DATA_DIR}/WINE32
+else
+	echo "---WINE workdirectory found---"
+fi
+echo "---Checking if WINE is properly installed---"
+if [ ! -d ${DATA_DIR}/WINE32/drive_c/windows ]; then
+	echo "---Setting up WINE---"
+	cd ${DATA_DIR}
+	winecfg > /dev/null 2>&1
+	sleep 15
+else
+	echo "---WINE properly set up---"
+fi
+
 echo "---Sleep zZz---"
 sleep infinity
+
+cd ${DATA_DIR}
+wine iw4x.exe -dedicated ${GAME_PARAMS}
