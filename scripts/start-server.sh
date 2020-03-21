3bcc1bb41f09908058922807185608cce3e3499f
ich777 <ich777@noreply.example.org>
2020-03-21 21:29:35 +0100

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -2,6 +2,7 @@
 echo "---Checking if IW4x is installed---"
 if [ ! -f ${DATA_DIR}/iw4x.exe ]; then
 	echo "---IW4x not found, downloading---"
+	cd ${DATA_DIR}
 	if wget -q -nc --show-progress --progress=bar:force:noscroll -O iw4x_files.zip "${IW4X_DL_URL}" ; then
 		echo "---Successfully downloaded IW4x!---"
 	else
@@ -15,6 +16,7 @@ fi
 echo "---Checking if IW4x DLC is installed---"
 if [ ! -f ${DATA_DIR}/iw4x.exe ]; then
 	echo "---IW4x not found, downloading---"
+	cd ${DATA_DIR}
 	if wget -q -nc --show-progress --progress=bar:force:noscroll -O iw4x_dlc.zip "${IW4X_DLC_URL}" ; then
 		echo "---Successfully downloaded IW4x DLC!---"
 	else
